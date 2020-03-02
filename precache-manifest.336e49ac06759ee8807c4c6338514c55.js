29a2f269c6d90a6f27a346119f048a6b922f1e86
Sara Ahlers <sara.ahlers@inxsql.com>
2020-03-02 15:18:07 -0500

    Updates

---
+++ precache-manifest.336e49ac06759ee8807c4c6338514c55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315e34ec2a7bfffdb8e85e4d8059a7bb",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "644fb7ceb161b63d951c",
+    "url": "/Portfolio/static/css/main.221cf535.chunk.css"
+  },
+  {
+    "revision": "b2042cb32f309c917173",
+    "url": "/Portfolio/static/js/2.76876d1f.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Portfolio/static/js/2.76876d1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644fb7ceb161b63d951c",
+    "url": "/Portfolio/static/js/main.1a3d9f8d.chunk.js"
+  },
+  {
+    "revision": "d377b0a30a68294ac928",
+    "url": "/Portfolio/static/js/runtime-main.931f019a.js"
+  },
+  {
+    "revision": "3cee23d8917a4ddfb01d5bf9976c8be3",
+    "url": "/Portfolio/static/media/demo.3cee23d8.jpg"
+  },
+  {
+    "revision": "7f4f7c1deb1d2a00d9c32b54693b15e5",
+    "url": "/Portfolio/static/media/fcb.7f4f7c1d.jpg"
+  },
+  {
+    "revision": "0f051bacf84e4e68003bafb05b606131",
+    "url": "/Portfolio/static/media/marine.0f051bac.jpg"
+  },
+  {
+    "revision": "359a3b9a0a9a2dc47e4f31bfeebbd896",
+    "url": "/Portfolio/static/media/shoppinglist.359a3b9a.jpg"
+  },
+  {
+    "revision": "fa04cf1e10b223c5bad4c2b4f57a1240",
+    "url": "/Portfolio/static/media/theatre.fa04cf1e.jpg"
+  },
+  {
+    "revision": "3494127e28dbf848cd6f5963f12a4b56",
+    "url": "/Portfolio/static/media/tripsnip.3494127e.jpg"
+  }
+]);
