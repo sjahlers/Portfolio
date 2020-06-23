4c19b5d8ee8aac891d8ce41e9b3ca10afc33949a
Sara Ahlers <sara.ahlers@inxsql.com>
2020-06-23 16:05:30 -0400

    Complete Marine project page. Crop INxSQL photos.

--- src/components/projects/inxsql.js
+++ src/components/projects/inxsql.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import demo from '../../img/demo.jpg';
 import demoMobile from '../../img/demo-mobile.jpg';
-import demoProducts from '../../img/demo-products.png';
-import demoAccount from '../../img/demo-account.png';
+import demoProducts from '../../img/demo-products.jpg';
+import demoAccount from '../../img/demo-account.jpg';
 
 class Inxsql extends React.Component {
   render() {
@@ -18,7 +18,7 @@ class Inxsql extends React.Component {
           <div className="col-12 col-lg-6 col-xl-4 font-md">
             <p className="font-lg">INxSQL Software is an ERP Distribution Software designed for wholesalers, distributors and industrial resellers. INxSQL's e-commerce website allows businesses to sell their inventory online, allows their customers to manage their account, and reconnects it all back into INxSQL.</p>
             <p>INxSQL's e-commerce templates needed a modern update with an enhanced user experience. I gave it a complete redesign including modern fonts, colors, and mobile-friendly layouts.</p>
-            <p><i class="fas fa-tools" aria-hidden="true"></i> Toolbox:</p>
+            <p><i className="fas fa-tools" aria-hidden="true"></i> Toolbox:</p>
               <ul>
                 <li>Sass</li>
                 <li>Bootstrap 4 Grid</li>
@@ -36,10 +36,13 @@ class Inxsql extends React.Component {
 
         <div className="row">
           <div className="col-12 col-md-4">
+            <p className="caption">Homepage mobile view</p>
             <img src={demoMobile} className="img-responsive screenshot" alt="INxSQL Mobile View" title="INxSQL Mobile View"/>
           </div>
           <div className="col-12 col-md-8">
-            <img src={demoProducts} className="img-responsive screenshot" alt="INxSQL Mobile View" title="INxSQL Mobile View"/>
+            <p className="caption">Product list page with product filters</p>
+            <img src={demoProducts} className="img-responsive screenshot" alt="INxSQL Products Listing" title="INxSQL Products Listing"/>
+            <p className="caption">Account dashboard page</p>
             <img src={demoAccount} className="img-responsive screenshot" alt="INxSQL Account Page" title="INxSQL Account Page"/>
           </div>
         </div>
--- src/components/projects/marine.js
+++ src/components/projects/marine.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import marine from '../../img/marine.jpg';
-
+import marine2 from '../../img/marine2.jpg';
+import marineHover from '../../img/marine-hover.jpg';
 
 class Marine extends React.Component {
   render() {
@@ -14,8 +15,9 @@ class Marine extends React.Component {
 
         <div className="row">
           <div className="col-12 col-lg-6 col-xl-4 font-md">
-            <p className="font-lg">Marine Fasteners is a full e-commerce site built upon INxSQL's web templates. Marine was using a Wordpress site and wanted the same look for their new e-commerce site on a tight timeframe. I recreated the content and appearance of their old site, making the transition to e-commerce seamless for their users.</p>            
-            <p><i class="fas fa-tools" aria-hidden="true"></i> Toolbox:</p>
+            <p className="font-lg">Marine Fasteners is a full e-commerce site built upon INxSQL's web templates.</p>
+            <p>Marine Fasteners wanted to convert their Wordpress site to e-commerce on INxSQL's platform. I recreated the content and appearance of their former site, making the transition to e-commerce seamless for their customers.</p>
+            <p><i className="fas fa-tools" aria-hidden="true"></i> Toolbox:</p>
               <ul>
                 <li>Sass</li>
                 <li>Bootstrap 3 Grid</li>
@@ -32,8 +34,15 @@ class Marine extends React.Component {
         </div>
 
         <div className="row">
-
+          <div className="col-12 col-md-6">
+            <p className="caption">Vivid photos with overlay content and informational graphic row</p>
+            <img src={marine2} className="img-responsive screenshot" alt="Marine Icon Row" title="Marine Icon Row"/>
           </div>
+          <div className="col-12 col-md-6">
+            <p className="caption">Photo links with color overlay and hover effect</p>
+            <img src={marineHover} className="img-responsive screenshot" alt="Marine Photo Link Hover Effect" title="Marine Photo Link Hover Effect"/>
+          </div>
+        </div>
       </div>
     )
   }
